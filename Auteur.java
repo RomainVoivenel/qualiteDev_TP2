affe0d425a39cfd476b52cb27c58a4d4c0833177
Romain Voivenel <romain.voivenel@etu.univ-orleans.fr>
2024-02-01 08:52:01 +0100

    Question 1 : Modification de Auteur.java

--- Auteur.java
+++ Auteur.java
@@ -1,23 +1,28 @@
+import java.util.Map;
+import java.util.HashMap;
+
 public class Auteur{
     private String nom;
-    private int qualiteTragedie;
-    private String citationTragedie;
-    private int qualiteComedie;
-    private String citationComedie;
-    private int qualiteDrame;
-    private String citationDrame;
+    Map<String, Integer> perfTragedie;
+    Map<String, Integer> perfComedie;
+    Map<String, Integer> perfDrame;
 
     public Auteur(String nom, int qualiteTragedie, String citationTragedie, int qualiteComedie, String citationComedie, int qualiteDrame, String citationDrame){
         this.nom = nom;
-        this.qualiteTragedie = qualiteTragedie;
-        this.citationTragedie = citationTragedie;
-        this.qualiteComedie = qualiteComedie;
-        this.citationComedie = citationComedie;
-        this.qualiteDrame = qualiteDrame;
-        this.citationDrame = citationDrame;
+        this.perfTragedie = new HashMap<>();
+        this.perfComedie = new HashMap<>();
+        this.perfDrame = new HashMap<>();
+        this.perfTragedie.put(citationTragedie,qualiteTragedie);
+        this.perfComedie.put(citationComedie,qualiteComedie);
+        this.perfDrame.put(citationDrame,qualiteDrame);
 
     }
-    public int getQualiteTragedie(){
-        return this.qualiteTragedie;
+    public int getQualiteTragedie(String citationTragedie){
+        return this.perfTragedie.get(citationTragedie);
+    }
+    @Override
+    public String toString(){
+        String s1 = "L'honorable ";
+        return s1.concat(this.nom);
     }
 }
