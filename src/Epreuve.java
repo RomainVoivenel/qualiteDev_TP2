7f38f856ffb3813ffaf17baf9bba863910d9b708
Romain Voivenel <romain.voivenel@etu.univ-orleans.fr>
2024-02-08 12:25:10 +0100

    Modification de Epreuve.java Ajout de TournoiJournee.java

--- src/Epreuve.java
+++ src/Epreuve.java
@@ -1,29 +1,23 @@
 public class Epreuve {
-    private Auteur auteur1;
-    private Auteur auteur2;
     private Style style;
     private Journee momentJournee;
     private int nbSpectateur;
-    public Epreuve(Auteur auteur1, Auteur auteur2, Style style, Journee momentJournee){
-        this.auteur1 = auteur1;
-        this.auteur2 = auteur2;
+    public Epreuve(Style style, Journee momentJournee){
         this.style = style;
         this.momentJournee = momentJournee;
     }
-    public int scoreAuteur1(){
-        return auteur1.qualiteStyle(style)*this.nbSpectateur;
+    public int scoreAuteur(Auteur auteur){
+        return auteur.qualiteStyle(style)*this.nbSpectateur;
     }
-    public int scoreAuteur2(){
-        return auteur2.qualiteStyle(style)*this.nbSpectateur;
-    }
+
     public Auteur combat(Auteur auteur1, Auteur auteur2){
-        if (this.scoreAuteur1()>this.scoreAuteur2()){
-            return this.auteur1;
+        if (this.scoreAuteur(auteur1)>this.scoreAuteur(auteur2)){
+            return auteur1;
         }
-        return this.auteur2;
+        return auteur2;
     }
     @Override
     public String toString(){
-        return "Combat entre " + this.auteur1 + " et " + this.auteur2 + " dans le style " + this.style + " durant " + this.momentJournee + " avec " + this.nbSpectateur + ". Le gagnant est " + this.combat(this.auteur1, this.auteur2);
+        return "Combat dans le style " + this.style + " durant " + this.momentJournee + " avec " + this.nbSpectateur;
     }
 }
