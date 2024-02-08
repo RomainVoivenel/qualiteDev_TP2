567af602b13e8bac38295fae5c6f669c82a7ea46
Romain Voivenel <romain.voivenel@etu.univ-orleans.fr>
2024-02-08 12:04:49 +0100

    Modification de Epreuve.java : Ajout de toString() Modification des fonctions

--- src/Epreuve.java
+++ src/Epreuve.java
@@ -3,6 +3,7 @@ public class Epreuve {
     private Auteur auteur2;
     private Style style;
     private Journee momentJournee;
+    private int nbSpectateur;
     public Epreuve(Auteur auteur1, Auteur auteur2, Style style, Journee momentJournee){
         this.auteur1 = auteur1;
         this.auteur2 = auteur2;
@@ -10,12 +11,19 @@ public class Epreuve {
         this.momentJournee = momentJournee;
     }
     public int scoreAuteur1(){
-        return 0;
+        return auteur1.qualiteStyle(style)*this.nbSpectateur;
     }
     public int scoreAuteur2(){
-        return 0;
+        return auteur2.qualiteStyle(style)*this.nbSpectateur;
     }
-    public Auteur gagnant(){
-        return this.auteur1;
+    public Auteur combat(Auteur auteur1, Auteur auteur2){
+        if (this.scoreAuteur1()>this.scoreAuteur2()){
+            return this.auteur1;
+        }
+        return this.auteur2;
+    }
+    @Override
+    public String toString(){
+        return "Combat entre " + this.auteur1 + " et " + this.auteur2 + " dans le style " + this.style + " durant " + this.momentJournee + " avec " + this.nbSpectateur + ". Le gagnant est " + this.combat(this.auteur1, this.auteur2);
     }
 }
